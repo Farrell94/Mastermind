475fe0b0c91768923d2c1ab53f25fdcc530992d2
nellybabillon <nelly.advert@gmail.com>
2023-01-05 15:52:49 +0100

    Instructions JS

---
+++ assets/js/mind.js
@@ -0,0 +1,9 @@
+// Créer variables pour chaque couleur de pions
+
+// Créer fonctions pour placer les pions aux emplacements en drag and drop
+
+// Créer fonction vérif des couleurs
+
+// Créer un random pour le choix des couleurs de l'IA
+
+
