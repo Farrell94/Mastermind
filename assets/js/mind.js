6a42be3e9730430985631145fe82e1165257658b
nellybabillon <nelly.advert@gmail.com>
2023-01-09 11:30:05 +0100

    MAJ JS
    RandomColor OK
    Output dans le HTML
    Division des lignes en tableau

--- assets/js/mind.js
+++ assets/js/mind.js
@@ -7,7 +7,7 @@ let emptyPionDecodeur = document.querySelectorAll('.decodeur')
 let pion = document.querySelectorAll('pion')
 
 
-let couleur = ['rouge','vert','bleu','jaune','violet','rose']
+
 let bleu = document.querySelectorAll('.bleu')
 let rouge = document.querySelectorAll('.rouge')
 let vert = document.querySelectorAll('.vert')
@@ -21,48 +21,59 @@ let almost = document.querySelectorAll('.almost')
 
 // Transformer Nodelist en tableau
 
-let allPionsArray = [...emptyPionGame]
-// Diviser les pions par rangées
+let allPionsGameArray = [...emptyPionGame]
+let allPionsDecodeurArray = [...emptyPionDecodeur]
+// Divise les pions par rangées
 
-function splitRange(){
-	
-	console.log(allPionsArray)
-	for (let i = 0; i < 4; i++) {
-	  	let row = allPionsArray.slice(0,4)
-		console.log(row)
-	}
+function splitRangeGame(){
+
+	for (let i = 0; i < 10; i++) {
+	  	let startRang = i*4
+	  	let endRange = (i+1)*4
+	  	let rowG = allPionsGameArray.slice(startRang,endRange)
+	  	let rowD = allPionsDecodeurArray.slice(startRang,endRange)
+
+			console.log(rowG,rowD)
+		}
 }
 
-splitRange();
-
+splitRangeGame();
 
 // Créer fonctions pour placer les pions aux emplacements en drag and drop
 
 
-  function dragstart_handler(ev) {
-    // Add the target element's id to the data transfer object
-    ev.dataTransfer.setData("text/plain", ev.target.id);
-  }
+  // function dragstart_handler(ev) {
+  //   // Add the target element's id to the data transfer object
+  //   ev.dataTransfer.setData("text/plain", ev.target.id);
+  // }
 
-  window.addEventListener("DOMContentLoaded", () => {
-    // Get the element by id
-    // Add the ondragstart event listener
-    pion.addEventListener("dragstart", dragstart_handler);
-  });
-
-
-// Créer fonction vérif des couleurs
-
-// function checkColor(){
-
-// }
+  // window.addEventListener("DOMContentLoaded", () => {
+  //   // Get the element by id
+  //   // Add the ondragstart event listener
+  //   pion.addEventListener("dragstart", dragstart_handler);
+  // });
 
 
 // Créer un random pour le choix des couleurs de l'IA
 
+let couleurDispo = ['rouge','vert','bleu','jaune','violet','rose']
+let couleurIA = []
+
 function randomColor(){
-	for(let i=0;i<=5;i++){
-		console.log(couleur[Math.floor(Math.random()* 6)])
+	for(let i=0;i<4;i++){
+		let randomCol = couleurDispo[Math.floor(Math.random()* 6)]
+		couleurIA.push(randomCol)
 	}
+	console.log(couleurIA)
 }
 randomColor();
+
+document.querySelector('.randomcolor').textContent = couleurIA
+
+// Créer fonction vérif des couleurs
+
+function checkColor(){
+	let colorIA = []
+	
+}
+
